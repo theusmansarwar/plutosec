151949c954de4d021b816fbe3560a8e26656bb9e
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-05-05 19:01:29 +0500

    json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',  // Enable static export
+  };
+  
+  export default nextConfig;
+  
