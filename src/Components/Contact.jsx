fe3eb7c3dafcf275f99716502914f0bdcb658038
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-05-12 15:50:07 +0500

    captcha

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -250,7 +250,7 @@ const Contact = () => {
         <ReCAPTCHA
           ref={recaptchaRef}
           sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-          size="invisible"
+        
         />
         <button className="send-btn" onClick={handleSubmit}>
           Send Message
